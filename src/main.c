f53a0735985aaad8ebb1c5ac12a7d98cb230abc5
David Gonzalez Martin <davidgmbb@gmail.com>
2025-10-11 12:36:58 +0200

    Add graphics capabilities

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
 #include <lib.h>
+#include <limine.h>
+
+#include <limine.h>
+
+__attribute__((used, section(".limine_requests")))
+static volatile LIMINE_BASE_REVISION(3);
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+__attribute__((used, section(".limine_requests_start")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+__attribute__((used, section(".limine_requests_end")))
+static volatile LIMINE_REQUESTS_END_MARKER;
 
 #define QEMU_EXIT_PORT 0xF4
 #define QEMU_EXIT_SUCCESS 0x00
@@ -24,5 +40,23 @@ LOCAL u8 inb(u16 port)
 
 [[noreturn]] EXPORT void _start()
 {
+    if (LIMINE_BASE_REVISION_SUPPORTED == false)
+    {
+        qemu_exit(1);
+    }
+
+    if ((framebuffer_request.response == 0) | (framebuffer_request.response->framebuffer_count < 1))
+    {
+        qemu_exit(1);
+    }
+
+    let framebuffer = framebuffer_request.response->framebuffers[0];
+
+    for (u64 i = 0; i < 100; i++)
+    {
+        volatile u32 *fb_ptr = framebuffer->address;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
     qemu_exit(0);
 }
