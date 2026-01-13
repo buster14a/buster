4676dd700ea66beee588577e6fe9ef1d4d1288b8
David Gonzalez Martin <davidgmbb@gmail.com>
2026-01-13 18:56:44 +0100

    Displace build flags to the .c file

--- src/buster/build/build_common.c
+++ src/buster/build/build_common.c
@@ -8,6 +8,115 @@ BUSTER_IMPL BuildTarget build_target_native = {
 #define BUILD_DEFINE(s, v) (SOs("-D" s "=" #v))
 #define BUILD_BOOLEAN_DEFINE(b, s) ((b) ? BUILD_DEFINE(#s, 1) : BUILD_DEFINE(#s, 0))
 
+BUSTER_GLOBAL_LOCAL StringOs enable_warning_flags[] = {
+    SOs("-Wall"),
+    SOs("-Wextra"),
+    SOs("-Wpedantic"),
+    SOs("-pedantic"),
+    SOs("-Wconversion"),
+    SOs("-Wstrict-overflow=5"),
+    SOs("-Woverflow"),
+    SOs("-Wshift-overflow"),
+    SOs("-Walloca"),
+    SOs("-Warray-bounds-pointer-arithmetic"),
+    SOs("-Wassign-enum"),
+    SOs("-Wbool-conversion"),
+    SOs("-Wbool-operation"),
+    SOs("-Wcomma"),
+    SOs("-Wconditional-uninitialized"),
+    SOs("-Wdangling"),
+    SOs("-Wdouble-promotion"),
+    SOs("-Wenum-compare-conditional"),
+    SOs("-Wenum-too-large"),
+    SOs("-Wexperimental-lifetime-safety"),
+    SOs("-Wfixed-point-overflow"),
+    SOs("-Wflag-enum"),
+    SOs("-Wformat"),
+    SOs("-Wfortify-source"),
+    SOs("-Wfour-char-constants"),
+    SOs("-Whigher-precision-for-complex-division"),
+    SOs("-Wimplicit"),
+    SOs("-Wimplicit-fallthrough"),
+    SOs("-Wimplicit-fallthrough-per-function"),
+    SOs("-Wimplicit-float-conversion"),
+    SOs("-Wimplicit-int-conversion"),
+    SOs("-Wimplicit-void-ptr-cast"),
+    SOs("-Winfinite-recursion"),
+    SOs("-Winvalid-utf8"),
+    SOs("-Wlarge-by-value-copy"),
+    SOs("-Wlinker-warnings"),
+    SOs("-Wloop-analysis"),
+    SOs("-Wmain"),
+    SOs("-Wmisleading-indentation"),
+    SOs("-Wmissing-braces"),
+    SOs("-Wmissing-noreturn"),
+    SOs("-Wnon-power-of-two-alignment"),
+    SOs("-Woption-ignored"),
+    SOs("-Woverlength-strings"),
+    SOs("-Wpacked"),
+    SOs("-Wpadded"),
+    SOs("-Wparentheses"),
+    SOs("-Wpedantic-macros"),
+    SOs("-Wpointer-arith"),
+    SOs("-Wpragma-pack"),
+    SOs("-Wpragma-pack-suspicious-include"),
+    SOs("-Wpragmas"),
+    SOs("-Wread-only-types"),
+    SOs("-Wredundant-parens"),
+    SOs("-Wreserved-identifier"),
+    SOs("-Wreserved-macro-identifier"),
+    SOs("-Wreserved-module-identifier"),
+    SOs("-Wself-assign"),
+    SOs("-Wself-assign-field"),
+    SOs("-Wshadow"),
+    SOs("-Wshadow-all"),
+    SOs("-Wshadow-field"),
+    SOs("-Wshift-bool"),
+    SOs("-Wshift-sign-overflow"),
+    SOs("-Wsigned-enum-bitfield"),
+    SOs("-Wtautological-compare"),
+    SOs("-Wtype-limits"),
+    SOs("-Wtautological-constant-in-range-compare"),
+    SOs("-Wthread-safety"),
+    SOs("-Wuninitialized"),
+    SOs("-Wunaligned-access"),
+    SOs("-Wunique-object-duplication"),
+    SOs("-Wunreachable-code"),
+    SOs("-Wunreachable-code-return"),
+    SOs("-Wvector-conversion"),
+};
+
+BUSTER_GLOBAL_LOCAL StringOs disable_warning_flags[] = {
+    SOs("-Wno-language-extension-token"),
+    SOs("-Wno-gnu-auto-type"),
+    SOs("-Wno-gnu-empty-struct"),
+    SOs("-Wno-bitwise-instead-of-logical"),
+    SOs("-Wno-unused-function"),
+    SOs("-Wno-gnu-flexible-array-initializer"),
+    SOs("-Wno-missing-field-initializers"),
+    SOs("-Wno-pragma-once-outside-header"),
+    SOs("-Wno-zero-length-array"),
+    SOs("-Wno-gnu-zero-variadic-macro-arguments"),
+    SOs("-Wno-gnu-statement-expression-from-macro-expansion"),
+};
+
+BUSTER_GLOBAL_LOCAL StringOs f_flags[] = {
+    SOs("-fwrapv"),
+    SOs("-fno-strict-aliasing"),
+    SOs("-funsigned-char"),
+    SOs("-fno-exceptions"),
+    SOs("-fno-rtti"),
+};
+
+BUSTER_GLOBAL_LOCAL StringOs include_flags[] = {
+    SOs("-Isrc"),
+};
+
+BUSTER_GLOBAL_LOCAL StringOs std_flags[] = {
+    SOs("-std=gnu2x"),
+};
+
+
 BUSTER_IMPL StringOsList build_compile_link_arguments(Arena* arena, const CompileLinkOptions * const restrict options)
 {
     // Forced to do it so early because we would need another arena here otherwise (since arena is used for the argument builder)
