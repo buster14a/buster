44d5c5c1c28c9d6d8aaad965e2002443ec526292
David Gonzalez Martin <davidgmbb@gmail.com>
2026-03-01 10:00:20 +0100

    Better understanding of X events

--- src/buster/build/build_common.c
+++ src/buster/build/build_common.c
@@ -243,6 +243,7 @@ BUSTER_IMPL StringOsList build_compile_link_arguments(Arena* arena, const Compil
         string_os_list_builder_append(builder, BUILD_BOOLEAN_DEFINE(options->fuzz, BUSTER_FUZZING));
         string_os_list_builder_append(builder, BUILD_BOOLEAN_DEFINE(options->use_io_ring, BUSTER_USE_IO_RING));
         string_os_list_builder_append(builder, BUILD_BOOLEAN_DEFINE(options->include_tests, BUSTER_INCLUDE_TESTS));
+        string_os_list_builder_append(builder, BUILD_BOOLEAN_DEFINE(options->optimize, BUSTER_OPTIMIZE));
 
         string_os_list_builder_append(builder, march_string_os);
 
--- src/buster/window.h
+++ src/buster/window.h
@@ -26,7 +26,7 @@ BUSTER_DECL OsWindowingHandle* os_windowing_initialize();
 BUSTER_DECL void os_windowing_deinitialize(OsWindowingHandle* windowing);
 BUSTER_DECL OsWindowHandle* os_window_create(OsWindowingHandle* windowing, OsWindowCreate create);
 BUSTER_DECL OsWindowSize os_window_get_framebuffer_size(OsWindowingHandle* windowing, OsWindowHandle* os_window);
-BUSTER_IMPL OsWindowingEventList os_windowing_poll_events(Arena* arena, OsWindowingHandle* windowing);
+BUSTER_DECL OsWindowingEventList os_windowing_poll_events(Arena* arena, OsWindowingHandle* windowing);
 
 BUSTER_DECL void* native_windowing_handle_from_os_windowing_handle(OsWindowingHandle* windowing);
 BUSTER_DECL void* native_window_handle_from_os_window_handle(OsWindowHandle* window);
