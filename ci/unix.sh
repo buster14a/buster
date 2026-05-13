1b08a25136f109725a0d006574388d27fc64734d
David Gonzalez Martin <davidgmbb@gmail.com>
2026-05-14 01:18:07 +0200

    Do not test for an extra unity build option
    Unity build is only true when release

--- ci/unix.sh
+++ ci/unix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-BUSTER_BUILD_DIRECTORY=build_ci-cc_${BUSTER_CC}-optimize_${BUSTER_OPTIMIZE}-sanitize_${BUSTER_SANITIZE}-fuzz_${BUSTER_FUZZ}-unity_build_${BUSTER_UNITY_BUILD}
+BUSTER_BUILD_DIRECTORY=build_ci-cc_${BUSTER_CC}-optimize_${BUSTER_OPTIMIZE}-sanitize_${BUSTER_SANITIZE}-fuzz_${BUSTER_FUZZ}
 source ./generate.sh
 source ./build.sh --verbose test_all
