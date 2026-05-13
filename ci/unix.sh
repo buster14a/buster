31ef34a8e00443ae9a301869de3786cc58facb7b
David Gonzalez Martin <davidgmbb@gmail.com>
2026-05-13 20:22:08 +0200

    Create separate script

--- ci/unix.sh
+++ ci/unix.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euox pipefail
 
-source generate.sh
-source build.sh test_all
+BUSTER_BUILD_DIRECTORY=build_ci-cc_${BUSTER_CC}-optimize_${BUSTER_OPTIMIZE}-sanitize_${BUSTER_SANITIZE}-fuzz_${BUSTER_FUZZ}-unity_build_${BUSTER_UNITY_BUILD}
+source ./generate.sh
+source ./build.sh test_all
