f064016aabc4676d807d823224345ba4c8eacbc5
David Gonzalez Martin <davidgmbb@gmail.com>
2026-05-14 23:35:57 +0200

    Implement basic Zig support

--- ci/unix.sh
+++ ci/unix.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-BUSTER_BUILD_DIRECTORY=build_ci-cc_${BUSTER_CC}-optimize_${BUSTER_OPTIMIZE}-sanitize_${BUSTER_SANITIZE}-fuzz_${BUSTER_FUZZ}
+BUSTER_CC_DIRECTORY=${BUSTER_CC//[^A-Za-z0-9_.-]/_}
+BUSTER_BUILD_DIRECTORY="build_ci-cc_${BUSTER_CC_DIRECTORY}-optimize_${BUSTER_OPTIMIZE}-sanitize_${BUSTER_SANITIZE}-fuzz_${BUSTER_FUZZ}"
 source ./generate.sh
 source ./build.sh --verbose test_all
