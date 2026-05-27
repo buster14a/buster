5ef0c09a8a9fa9d08a29c5fccb695e351465cf2e
David Gonzalez Martin <davidgmbb@gmail.com>
2026-05-27 18:07:14 +0200

    Switch back to TCC

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 mkdir -p build
-clang -Isrc -Wall -Werror -Wno-unused-function -g build.c -o build/build
+tcc -Isrc -Wall -Werror -Wno-unused-function -g build.c -o build/build
 build/build $@
