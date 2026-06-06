54474cf464fc004e5d3cca51d1488ad8a5363896
David Gonzalez Martin <davidgmbb@gmail.com>
2026-06-06 16:40:50 +0200

    Delete duplicated code

--- src/buster/assertion.c
+++ src/buster/assertion.c
@@ -4,23 +4,6 @@
 
 BUSTER_NORETURN BUSTER_COLD void buster_failed_assertion(u32 line, String8 function_name, String8 file_path)
 {
-    string_print(S8("Assert failed at "));
-    string_print(function_name);
-    string_print(S8(" in "));
-    string_print(file_path);
-    string_print(S8(":"));
-
-    TemporalArena scratch = scratch_begin(0, 0);
-    string_format(scratch.arena, S8("{u32}"), line);
-
-    // TODO
-    // char8 buffer[128];
-    // let buffer_slice = (String8){ buffer, BUSTER_ARRAY_LENGTH(buffer) };
-    // let line_format = string8_format(buffer_slice, S8("{u32}"), line);
-    // string8_print(line_format);
-    // string8_print(S8("\n"));
-        
     os_fail_ex(line, function_name, file_path);
-    scratch_end(scratch);
 }
 
