e62ed34e6b162371175e1c5479c600376e6a7548
David Gonzalez Martin <davidgmbb@gmail.com>
2026-09-12 18:15:05 -0600

    tests: isolate generic AArch64 conditional forms

--- src/buster/tests/compiler/assembly/assembly_test.c
+++ src/buster/tests/compiler/assembly/assembly_test.c
@@ -5402,6 +5402,22 @@ UnitTestResult assembly_tests(UnitTestArguments* arguments)
     BUSTER_TEST(arguments, aarch64_inline_conditional.relocation_count == 1 &&
                                aarch64_inline_conditional.relocations[0].offset == 0 &&
                                aarch64_inline_conditional.relocations[0].kind == ASSEMBLY_RELOCATION_AARCH64_COMPAREBR19);
+    AssemblyEncodeResult aarch64_inline_compare = assembly_encode(
+        arguments->arena, S8("cmp w9, #1\n"), (AssemblyEncodeOptions){.target = aarch64_inline_baseline_target});
+    static u8 const expected_aarch64_inline_compare[] = {0x3f, 0x05, 0x00, 0x71};
+    BUSTER_TEST(arguments, aarch64_inline_compare.diagnostic_count == 0 &&
+                               aarch64_inline_compare.bytes.length == sizeof(expected_aarch64_inline_compare) &&
+                               memcmp(aarch64_inline_compare.bytes.pointer, expected_aarch64_inline_compare,
+                                      sizeof(expected_aarch64_inline_compare)) == 0);
+    AssemblyEncodeResult aarch64_inline_bcond = assembly_encode(
+        arguments->arena, S8("b.eq .Lbuster.inline.asm.0.2\n"), (AssemblyEncodeOptions){.target = aarch64_inline_baseline_target});
+    static u8 const expected_aarch64_inline_bcond[] = {0x00, 0x00, 0x00, 0x54};
+    BUSTER_TEST(arguments, aarch64_inline_bcond.diagnostic_count == 0 &&
+                               aarch64_inline_bcond.bytes.length == sizeof(expected_aarch64_inline_bcond) &&
+                               memcmp(aarch64_inline_bcond.bytes.pointer, expected_aarch64_inline_bcond,
+                                      sizeof(expected_aarch64_inline_bcond)) == 0 &&
+                               aarch64_inline_bcond.relocation_count == 1 &&
+                               aarch64_inline_bcond.relocations[0].kind == ASSEMBLY_RELOCATION_AARCH64_CONDBR19);
     AssemblyEncodeResult aarch64_inline_multiple = assembly_encode(
         arguments->arena,
         S8("cbz w9, .Lbuster.inline.asm.0.1\ncmp w9, #1\nb.eq .Lbuster.inline.asm.0.2\n"),
