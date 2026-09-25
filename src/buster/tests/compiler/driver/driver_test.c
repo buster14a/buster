5d7101f285396778557ef3ea3a91c0dc6650acba
David <davidgmbb@gmail.com>
2026-09-25 10:11:23 -0500

    test(c): split aggregate fixture under host literal limit (#1209)

--- src/buster/tests/compiler/driver/driver_test.c
+++ src/buster/tests/compiler/driver/driver_test.c
@@ -15718,7 +15718,7 @@ UnitTestResult compiler_driver_tests(UnitTestArguments* arguments)
             scratch_end(fixture_temporary);
         }
     }
-    String8 c_flat_initializer_source = S8(
+    String8 c_flat_initializer_prefix = S8(
         "struct Pair\n"
         "{\n"
         "    int first;\n"
@@ -15828,7 +15828,8 @@ UnitTestResult compiler_driver_tests(UnitTestArguments* arguments)
         "    return direct.pair.first != 79 || direct.pair.second != 83 || direct.tail != 89 || parenthesized.pair.first != 97 ||\n"
         "           parenthesized.pair.second != 101 || parenthesized.tail != 103;\n"
         "}\n"
-        "\n"
+        "\n");
+    String8 c_flat_initializer_suffix = S8(
         "static int nested_overrides(void)\n"
         "{\n"
         "    struct NamedOuter leaf = {.pair = {1, 2}, .pair.first = 3};\n"
@@ -15860,6 +15861,7 @@ UnitTestResult compiler_driver_tests(UnitTestArguments* arguments)
         "{\n"
         "    return named_local() || named_literal() || anonymous_local() || array_local() || union_local() || aggregate_expression() || nested_overrides();\n"
         "}\n");
+    String8 c_flat_initializer_source = string_format(arguments->arena, S8("{S8}{S8}"), c_flat_initializer_prefix, c_flat_initializer_suffix);
     String8 c_flat_initializer_frontends[] = {S8("-fno-frontend-ssa"), S8("-ffrontend-ssa")};
     String8 c_flat_initializer_optimizations[] = {S8("-O0"), S8("-O2")};
     for (u64 frontend_index = 0; frontend_index < BUSTER_ARRAY_LENGTH(c_flat_initializer_frontends); frontend_index += 1)
